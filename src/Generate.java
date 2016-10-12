e96411020105ce29787a983f6798f84228ba0a08
Ronaldo Milfont <ronaldotpmilfont@gmail.com>
2016-10-12 20:52:13 -0300

    Remove DistTime from Generate
    Put the DistTime collection as a parameter of Generate and remove useless parameters from Generate.
    
    Commit:Ref
    
    TaskNumber:#24

--- src/Generate.java
+++ src/Generate.java
@@ -4,217 +4,149 @@ import java.util.*;
 
 public class Generate {
 
-	private FileOutputStream file;
-	private DataOutputStream dataOutput;
-	private Map<String, Integer> numberOfPacks = new HashMap<String, Integer>();
-	private int[][] nPcks;
+    private FileOutputStream file;
+    private DataOutputStream dataOutput;
+    private Map<String, Integer> numberOfPacks = new HashMap<>();
+    private int[][] nPcks;
+    private int warmupPcks;
+    private int numberOfPackets;
+    private int packetSize;
+    private int dimX, dimY, flitWidth;
+    private Integer[] nPackets;
+    private Integer[] nFlits;
+    private int sequenceNumberH = 0, sequenceNumberL = 1;
 
-	private int warmupPcks;
-	private double frequency;
-	private int packetSize, numberPackets;
-	private int dimX, dimY, flitWidth, flitClockCycles;
-	private Integer[] nPackets;
-	private Integer[] nFlits;
-	int priorityO;
+    public Generate(int dimX, int dimY, int flitWidth, int numberPackets, int warmupPcks, int packetSize) {
+        nPcks = new int[dimX * dimY][dimX * dimY];
+        this.warmupPcks= warmupPcks;
+        this.numberOfPackets = numberPackets;
+        this.dimX = dimX;
+        this.dimY = dimY;
+        this.flitWidth = flitWidth;
+        this.packetSize = packetSize;
 
-	int a = 0;
-	int b = 0;
+        nPackets = new Integer[dimX * dimY];
+        for (int i = 0; i < dimX * dimY; i++)
+            nPackets[i] = 0;
 
-	private String scenarioPath;
-	private Integer totalFlits = 0;
-	private int sequenceNumberH = 0, sequenceNumberL = 1;
+        nFlits = new Integer[dimX * dimY];
+        for (int i = 0; i < dimX * dimY; i++)
+            nFlits[i] = 0;
+    }
 
-	public Generate(int dimX, int dimY, int flitWidth,
-			int flitClockCycles, int numberPackets, int warmupPcks,
-			int packetSize, double frequency) {
+    public void writeTraffic(ArrayList<String> sinks, String scenarioPath, List<Integer> timeStamps) {
+        File diretory = new File(scenarioPath + File.separator + "In");
+        diretory.mkdirs();
+        for (int y = 0; y < dimY; y++) {
+            for (int x = 0; x < dimX; x++) {
+                try {
+                    // Create files for all routers
+                    file = new FileOutputStream(scenarioPath + File.separator
+                            + "In" + File.separator + "in"
+                            + Conversion.formatAddress(x, y, flitWidth) + ".txt");
+                    dataOutput = new DataOutputStream(file);
+                    // Format and print packets
+                    writeLinesPS(sinks, x, y, timeStamps);
+                    dataOutput.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
-		nPcks = new int[dimX * dimY][dimX * dimY];
-
-		this.warmupPcks= warmupPcks;
-		this.dimX = dimX;
-		this.dimY = dimY;
-		this.flitWidth = flitWidth;
-		this.flitClockCycles = flitClockCycles;
-		nPackets = new Integer[dimX * dimY];
-
-		for (int i = 0; i < dimX * dimY; i++)
-			nPackets[i] = 0;
-
-		nFlits = new Integer[dimX * dimY];
-		for (int i = 0; i < dimX * dimY; i++)
-			nFlits[i] = 0;
-
-		this.numberPackets = numberPackets;
-		this.packetSize = packetSize;
-		this.frequency = frequency;
-	}
-
-	public void writeTraffic(ArrayList<String> sinks, String target,
-			String Caminho, double rate) {
-		File diretory;
-		nPcks = new int[dimX * dimY][dimX * dimY];
-
-		scenarioPath = Caminho;
-		diretory = new File(scenarioPath + File.separator + "In");
-
-		diretory.mkdirs();
-
-		for (int y = 0; y < dimY; y++) {
-			for (int x = 0; x < dimX; x++) {
-				try {
-					// Create files for all routers
-					file = new FileOutputStream(scenarioPath + File.separator
-							+ "In" + File.separator + "in"
-							+ Conversion.formatAddress(x, y, flitWidth) + ".txt");
-
-					dataOutput = new DataOutputStream(file);
-
-					// Format and print packets
-					writeLinesPS(sinks, x, y, numberPackets, rate, warmupPcks);
-					
-
-					dataOutput.close();
-
-				}
-
-				catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-		}
-	}
-
-	public void writeLinesPS(ArrayList<String> sinks, int x,
-			int y, int numberPackets, double Rate, int warmUpPcks) {
-
-		String linha;
-
-		int sourceX, sourceY, iTarget;
-		int payloadSize = 0;
-
-		//System.out.println("warm up packs: "+warmupPcks);
-		int totalNPcks = numberPackets+2*warmupPcks;
-		String[] timestampHex;
-
-		DistTime distTime = new DistTime(flitClockCycles,
-				packetSize, totalNPcks, Rate);
-
-		List<Integer> timeStamps = distTime.uniform();
-
-		try {
-
-			for (int j = 0; j < totalNPcks; j++) {
-				linha = "";
-				/***************************************************************************************/
+    public void writeLinesPS(ArrayList<String> sinks, int sourceX, int sourceY, List<Integer> timeStamps) {
+        try {
+            String line;
+            int totalNPcks = numberOfPackets +2*warmupPcks;
+            for (int j = 0; j < totalNPcks; j++) {
+                line = "";
+                /***************************************************************************************/
 				/*
 				 * TIMESTAMP INICIAL
 				 * /*******************************************
 				 * *******************************************
 				 */
-				linha = linha.concat(Integer.toHexString(timeStamps.get(j)).toUpperCase() + " ");
-				
-
-				/***************************************************************************************/
+                line = line.concat(Integer.toHexString(timeStamps.get(j)).toUpperCase() + " ");
+                /***************************************************************************************/
 				/*
 				 * 1� FLIT = PRIORITY (HIGH) + TARGET (LOW) /******************
 				 * *******************************************
 				 * *************************
 				 */
-				// priority = Conversion.decimal_hexa(priorityO, (flitWidth /
-				// 8));
-
-				sourceX = x;
-				sourceY = y;
-				String target = sinks.get((x * totalNPcks) + (y * totalNPcks * dimX) + j);
-				
-				if(target.equals(Conversion.formatAddress(x, y, flitWidth)))
-					break;
-					
-				// Get the number of pcks per flux
-				String HashKey = sourceX + "." + sourceY + " " + target;
-				int sourceN = sourceX + sourceY * dimX;
-				int sinkN = vertexIndex(target, dimX, flitWidth);
-
-				if(!(j<warmupPcks || j>=(totalNPcks-warmupPcks)))
-					nPcks[sourceN][sinkN]++;
-
-				if(!(j<warmupPcks || j>=(totalNPcks-warmupPcks)))
-				{
-					if (!numberOfPacks.containsKey(HashKey)) {
-						numberOfPacks.put(HashKey, 1);
-					} else {
-						numberOfPacks.put(HashKey, numberOfPacks.get(HashKey) + 1);
-					}
-				}
-
-				//iTarget = Conversion.vertexIndex(target,dimX, FlitSize);
-				iTarget = sinkN;
-
-				linha = linha.concat(/* priority + */target + " ");
-				/***************************************************************************************/
+                // priority = Conversion.decimal_hexa(priorityO, (flitWidth /
+                // 8));
+                String target = sinks.get((sourceX * totalNPcks) + (sourceY * totalNPcks * dimX) + j);
+                if(target.equals(Conversion.formatAddress(sourceX, sourceY, flitWidth))) {
+                    break;
+                }
+                // Get the number of pcks per flux
+                String HashKey = sourceX + "." + sourceY + " " + target;
+                int sourceN = sourceX + sourceY * dimX;
+                int sinkN = vertexIndex(target, dimX, flitWidth);
+                if(!(j<warmupPcks || j>=(totalNPcks-warmupPcks))) {
+                    nPcks[sourceN][sinkN]++;
+                }
+                if(!(j<warmupPcks || j>=(totalNPcks-warmupPcks))) {
+                    if (!numberOfPacks.containsKey(HashKey)) {
+                        numberOfPacks.put(HashKey, 1);
+                    }
+                    else {
+                        numberOfPacks.put(HashKey, numberOfPacks.get(HashKey) + 1);
+                    }
+                }
+                //iTarget = Conversion.vertexIndex(target,dimX, FlitSize);
+                int iTarget = sinkN;
+                line = line.concat(/* priority + */target + " ");
+                /***************************************************************************************/
 				/*
 				 * 2o FLIT = SIZE /********************************************
 				 * ******************************************
 				 */
-
-				payloadSize = packetSize - 6; // 6 pq 2 s�o header e 4 s�o o
-												// timestampHex da rede inserido
-												// pelo fli
-				linha = linha.concat(addLineByte(
-						Integer.toHexString(payloadSize).toUpperCase(), " "));
-				
-
-				
-					nPackets[iTarget]++;
-
-				nFlits[iTarget] += packetSize;
-				totalFlits += packetSize;
-
-				/***************************************************************************************/
+                int payloadSize = packetSize - 6; // 6 pq 2 s�o header e 4 s�o o
+                // timestampHex da rede inserido
+                // pelo fli
+                line = line.concat(addLineByte(
+                        Integer.toHexString(payloadSize).toUpperCase(), " "));
+                nPackets[iTarget]++;
+                nFlits[iTarget] += packetSize;
+                /***************************************************************************************/
 				/*
 				 * 3o FLIT = SOURCE /******************************************
 				 * ********************************************
 				 */
 
-				linha = linha.concat(Conversion.formatAddress(x, y, flitWidth) + " ");
-				
-
-				/***************************************************************************************/
+                line = line.concat(Conversion.formatAddress(sourceX, sourceY, flitWidth) + " ");
+                /***************************************************************************************/
 				/*
 				 * 4o - 7o FLITS = TIMESTAMP HEXA /**************************
 				 * ************************************************************
 				 */
-				timestampHex = getTimestamp(Integer.toHexString(timeStamps.get(j)).toUpperCase());
-				linha = linha.concat(timestampHex[3] + " " + timestampHex[2]
-						+ " " + timestampHex[1] + " " + timestampHex[0] + " ");
-				
-
-				/***************************************************************************************/
+                String[] timestampHex = getTimestamp(Integer.toHexString(timeStamps.get(j)).toUpperCase());
+                line = line.concat(timestampHex[3] + " " + timestampHex[2]
+                        + " " + timestampHex[1] + " " + timestampHex[0] + " ");
+                /***************************************************************************************/
 				/*
 				 * 8o AND 9o FLIT = SEQUENCE NUMBER /************************
 				 * *************************************
 				 * *************************
 				 */
-				if(!(j<warmupPcks || j>=totalNPcks-warmupPcks))
-				{
-					linha = linha.concat(addLineByte(Integer.toHexString(sequenceNumberH).toUpperCase()," "));
-					linha = linha.concat(addLineByte(Integer.toHexString(sequenceNumberL).toUpperCase(),""));
-
-				 //incrementando o numero de sequencia
-					if (sequenceNumberL >= Math.pow(2, flitWidth)-1) {
-						sequenceNumberH++;
-						sequenceNumberL = 0;
-					} else
-						sequenceNumberL++;
-				}
-				else
-				{
-					linha = linha.concat(addLineByte(""," "));
-					linha = linha.concat(addLineByte("",""));
-				}
-				
-				/***************************************************************************************/
+                if(!(j<warmupPcks || j>=totalNPcks-warmupPcks)) {
+                    line = line.concat(addLineByte(Integer.toHexString(sequenceNumberH).toUpperCase()," "));
+                    line = line.concat(addLineByte(Integer.toHexString(sequenceNumberL).toUpperCase(),""));
+                    //incrementando o numero de sequencia
+                    if (sequenceNumberL >= Math.pow(2, flitWidth)-1) {
+                        sequenceNumberH++;
+                        sequenceNumberL = 0;
+                    } else
+                        sequenceNumberL++;
+                }
+                else {
+                    line = line.concat(addLineByte(""," "));
+                    line = line.concat(addLineByte("",""));
+                }
+                /***************************************************************************************/
 				/*
 				 * PAYLOAD
 				 * /*****************************************************
@@ -241,86 +173,86 @@ public class Generate {
 					}
 
 				}
-				linha = linha.concat(payload);
+				line = line.concat(payload);
 				*/
-				dataOutput.writeBytes(linha + "\r\n");
-			}
-		} catch (IOException e) {
-			System.out.println("Exception at WriteLinePS");
-		}
-	}
+                dataOutput.writeBytes(line + "\r\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Exception at WriteLinePS");
+        }
+    }
 
-	private static int vertexIndex(String vertex, int nRotX, int flitSize) {
-		int x = vertexAxisX(vertex, flitSize);
-		int y = vertexAxisY(vertex, flitSize);
-		return (y * nRotX + x);
-	}
+    private static int vertexIndex(String vertex, int nRotX, int flitSize) {
+        int x = vertexAxisX(vertex, flitSize);
+        int y = vertexAxisY(vertex, flitSize);
+        return (y * nRotX + x);
+    }
 
-	private static int vertexAxisX(String vertex, int flitSize) {
-		int nodeX = Integer.parseInt(vertex.substring(0, vertex.length() / 2), 16);
-		String nX = Conversion.zeroLeftPad(Integer.toBinaryString(nodeX), flitSize / 2);
-		return Integer.parseInt(nX,2);
-	}
+    private static int vertexAxisX(String vertex, int flitSize) {
+        int nodeX = Integer.parseInt(vertex.substring(0, vertex.length() / 2), 16);
+        String nX = Conversion.zeroLeftPad(Integer.toBinaryString(nodeX), flitSize / 2);
+        return Integer.parseInt(nX,2);
+    }
 
-	private static int vertexAxisY(String vertex, int flitSize) {
-		int nodeY = Integer.parseInt(vertex.substring(vertex.length() / 2), 16);
-		String nY = Conversion.zeroLeftPad(Integer.toBinaryString(nodeY), flitSize / 2);
-		return Integer.parseInt(nY,2);
-	}
+    private static int vertexAxisY(String vertex, int flitSize) {
+        int nodeY = Integer.parseInt(vertex.substring(vertex.length() / 2), 16);
+        String nY = Conversion.zeroLeftPad(Integer.toBinaryString(nodeY), flitSize / 2);
+        return Integer.parseInt(nY,2);
+    }
 
-	private String addLineByte(String data, String separador) {
-		if (data != null) {
-			int sizeEsperado = flitWidth / 4;
-			int sizeData = data.length();
-			if (sizeData > sizeEsperado)
-				System.err.println("Problema no tamanho do flit "+data);
-			else if (sizeData < sizeEsperado) {
-				for (int i = 0; i < (sizeEsperado - sizeData); i++)
-					data = "0" + data;
-			}
-			return (data + separador);
-		}
-		return separador;
-	}
+    private String addLineByte(String data, String separador) {
+        if (data != null) {
+            int sizeEsperado = flitWidth / 4;
+            int sizeData = data.length();
+            if (sizeData > sizeEsperado)
+                System.err.println("Problema no tamanho do flit "+data);
+            else if (sizeData < sizeEsperado) {
+                for (int i = 0; i < (sizeEsperado - sizeData); i++)
+                    data = "0" + data;
+            }
+            return (data + separador);
+        }
+        return separador;
+    }
 
-	private String[] getTimestamp(double value) {
-		String[] timestampHex = new String[4];
-		for (int i = 0; i < 4; i++) { // 4 eh o numero de flits
-										// correspondente ao timestampHex
-			timestampHex[i] = "";
-			for (int l = 0; l < (flitWidth / 4); l++) {
-				timestampHex[i] = Conversion.zeroLeftPad(Integer.toHexString((int) value % 16), 1)
-						+ timestampHex[i];
-				value = value / 16;
-			}
-		}
-		return timestampHex;
-	}
+    private String[] getTimestamp(double value) {
+        String[] timestampHex = new String[4];
+        for (int i = 0; i < 4; i++) { // 4 eh o numero de flits
+            // correspondente ao timestampHex
+            timestampHex[i] = "";
+            for (int l = 0; l < (flitWidth / 4); l++) {
+                timestampHex[i] = Conversion.zeroLeftPad(Integer.toHexString((int) value % 16), 1)
+                        + timestampHex[i];
+                value = value / 16;
+            }
+        }
+        return timestampHex;
+    }
 
-	private String[] getTimestamp(String value) {
-		String[] timestampHex = new String[4];
+    private String[] getTimestamp(String value) {
+        String[] timestampHex = new String[4];
 
-		value = Conversion.zeroLeftPad(value, flitWidth);
-		// System.out.println("value "+value);
-		for (int i = 0, j = flitWidth; i < 4; i++, j = j - (flitWidth / 4)) {
-			timestampHex[i] = value.substring(j - (flitWidth / 4), j);
-			// System.out.println("value["+i+"] "+value.substring(j-(flitWidth/4),j));
-		}
-		return timestampHex;
-	}
+        value = Conversion.zeroLeftPad(value, flitWidth);
+        // System.out.println("value "+value);
+        for (int i = 0, j = flitWidth; i < 4; i++, j = j - (flitWidth / 4)) {
+            timestampHex[i] = value.substring(j - (flitWidth / 4), j);
+            // System.out.println("value["+i+"] "+value.substring(j-(flitWidth/4),j));
+        }
+        return timestampHex;
+    }
 
-	// Print sourceXsourceY sinkXsinkY nOfPcks
-	public void printNofPcks(String path) {
-		try {
-			Formatter output = new Formatter(path);
+    // Print sourceXsourceY sinkXsinkY nOfPcks
+    public void printNofPcks(String path) {
+        try {
+            Formatter output = new Formatter(path);
 
-			for (int x = 0; x < (dimX * dimY); x++) {
-				for (int y = 0; y < (dimY * dimX); y++) {
-					output.format("%d \t", nPcks[x][y]);
-				}
-				output.format("\r\n");
-			}
-			output.close();
+            for (int x = 0; x < (dimX * dimY); x++) {
+                for (int y = 0; y < (dimY * dimX); y++) {
+                    output.format("%d \t", nPcks[x][y]);
+                }
+                output.format("\r\n");
+            }
+            output.close();
 
 			/*
 			FileWriter fw = new FileWriter(new File(path + "nPcksHash"));
@@ -330,13 +262,13 @@ public class Generate {
 			fw.close();
 			*/
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		// Re-initialize numberOfPacks to different OferredLoads
-		this.numberOfPacks.clear();
+        // Re-initialize numberOfPacks to different OferredLoads
+        this.numberOfPacks.clear();
 
-	}
+    }
 
 }
--- src/TrafficGen.java
+++ src/TrafficGen.java
@@ -1,87 +1,83 @@
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TrafficGen {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String caminho = ".";
-		int dimX = 8; // X dimension
-		int dimY = 8; // Y dimension
-		int n_pck = 100; // packets per core
-		double percentToWarmUp = 0.1;
-		int nFlits = 15; // #flits
-		int flitWidth = 16;
-		double freq = 50.0;
-		int flitClockCycles = 1; // on-off
-		ArrayList<Double> rates = new ArrayList<Double>();
+        String caminho = ".";
+        int dimX = 8; // X dimension
+        int dimY = 8; // Y dimension
+        int numberOfPackets = 100; // packets per core
+        double percentToWarmUp = 0.1;
+        int flitsPerPackage = 15; // #flits
+        int flitWidth = 16;
+        int flitClockCycles = 1; // on-off
+        ArrayList<Double> rates = new ArrayList<Double>();
 
-		if (args.length == 6) {
+        if (args.length == 6) {
+            dimX = Integer.parseInt(args[0]); // X dimension
+            dimY = Integer.parseInt(args[1]); // Y dimension
+            numberOfPackets = Integer.parseInt(args[2]); // # packets per core
+            flitsPerPackage = Integer.parseInt(args[3]); // # flits
+            flitWidth = Integer.parseInt(args[4]); // flit size
+            percentToWarmUp=0.1;
+            String[] inputRates = args[5].split(",");
+            for (String rate : inputRates)
+                rates.add(Double.parseDouble(rate));
+        } else {
+            for(int i = 1; i < 21; i++)
+                rates.add((double)i);
+        }
 
-			dimX = Integer.parseInt(args[0]); // X dimension
-			dimY = Integer.parseInt(args[1]); // Y dimension
-			n_pck = Integer.parseInt(args[2]); // # packets per core
-			nFlits = Integer.parseInt(args[3]); // # flits
-			flitWidth = Integer.parseInt(args[4]); // flit size
-			percentToWarmUp=0.1;
-			String[] inputRates = args[5].split(",");
+        int warmupPcks =(int)Math.ceil((double)numberOfPackets*percentToWarmUp);
+        int totalPcks = numberOfPackets+2*warmupPcks;
+        //String distrib = "random";
+        //String distrib = "1.0"; //Hotspot
+        // String distrib = "bitReversal";
+        String distrib = "butterfly";
+        // String distrib = "complemento";
+        // String distrib = "matrixTranspose";
+        // String distrib = "perfectShuffle";
+        if(flitsPerPackage < 13) {
+            System.err.println("Pacote muito pequeno. Assumindo 13 flits...");
+            flitsPerPackage = 13;
+        }
+        if(Math.pow(2, flitWidth) < flitsPerPackage-2) {
+            flitWidth = (int) Math.ceil(Math.log(flitsPerPackage-2)/Math.log(2)) + 1;
+            if(flitWidth%4 != 0) {
+                flitWidth = (flitWidth / 4 + 1) * 4;
+            }
+            System.err.println("Flit pequeno para pacotes de "+flitsPerPackage+ " flits. Assumindo "+flitWidth+" bits.");
+        }
+        if(Math.pow(2, flitWidth/2) < dimX || Math.pow(2, flitWidth/2) < dimY) {
+            int dim = (dimX > dimY) ? dimX : dimY;
+            flitWidth = (int) (2.0*Math.ceil(Math.log(dim)/Math.log(2)));
+            if(flitWidth%4 != 0) {
+                flitWidth = (flitWidth / 4 + 1) * 4;
+            }
+            System.err.println("Flit pequeno para rede "+dimX+"x"+dimY+ ". Assumindo "+flitWidth+" bits.");
+        }
+        if(Math.pow(2, 2*flitWidth) < numberOfPackets*dimX*dimY) {
+            flitWidth = (int) (Math.ceil(Math.log(numberOfPackets*dimX*dimY)/Math.log(2))/2.0) + 1;
+            if(flitWidth%4 != 0) {
+                flitWidth = (flitWidth / 4 + 1) * 4;
+            }
+            System.err.println("Flit pequeno para "+numberOfPackets+" pacotes. Assumindo "+flitWidth+" bits.");
+        }
 
-			for (String rate : inputRates)
-				rates.add(Double.parseDouble(rate));
-		} else {
-			for(int i = 1; i < 21; i++)
-				rates.add((double)i);
-		}
-
-		int warmupPcks =(int)Math.ceil((double)n_pck*percentToWarmUp);
-		int totalPcks = n_pck+2*warmupPcks;
-		int desX = dimX/2, desY = dimY/2; // for hot spot
-		 //String distrib = "random";
-		 //String distrib = "1.0"; //Hotspot 
-		// String distrib = "bitReversal";
-		String distrib = "butterfly";
-		// String distrib = "complemento";
-		// String distrib = "matrixTranspose";
-		// String distrib = "perfectShuffle";
-		if(nFlits < 13) {
-			System.err.println("Pacote muito pequeno. Assumindo 13 flits...");
-			nFlits = 13;
-		}
-		if(Math.pow(2, flitWidth) < nFlits-2) {
-			flitWidth = (int) Math.ceil(Math.log(nFlits-2)/Math.log(2)) + 1;
-			if(flitWidth%4 != 0) 
-				flitWidth = (flitWidth/4+1)*4;
-			System.err.println("Flit pequeno para pacotes de "+nFlits+ " flits. Assumindo "+flitWidth+" bits.");
-		}
-		if(Math.pow(2, flitWidth/2) < dimX || Math.pow(2, flitWidth/2) < dimY) {
-			int dim = (dimX > dimY) ? dimX : dimY;
-			flitWidth = (int) (2.0*Math.ceil(Math.log(dim)/Math.log(2)));
-			if(flitWidth%4 != 0) 
-				flitWidth = (flitWidth/4+1)*4;
-			System.err.println("Flit pequeno para rede "+dimX+"x"+dimY+ ". Assumindo "+flitWidth+" bits.");
-		}
-		if(Math.pow(2, 2*flitWidth) < n_pck*dimX*dimY) {
-			flitWidth = (int) (Math.ceil(Math.log(n_pck*dimX*dimY)/Math.log(2))/2.0) + 1;
-			if(flitWidth%4 != 0) 
-				flitWidth = (flitWidth/4+1)*4;
-			System.err.println("Flit pequeno para "+n_pck+" pacotes. Assumindo "+flitWidth+" bits.");
-		}
-
-		Generate gen = new Generate(dimX, dimY, flitWidth, flitClockCycles, n_pck, warmupPcks, nFlits, freq);
-
-		genSinks genS = new genSinks(dimX, dimY, flitWidth);
-
-		for (Double rate : rates) {
-			System.out.println("Generating the "+rate+"% rate.");
-			String path = caminho + File.separator + "F" + String.format("%03d", rate.intValue());
-			ArrayList<String> sinks = genS.doSinks(distrib, totalPcks);
-			gen.writeTraffic(sinks, distrib, path, rate);
-		}
-		gen.printNofPcks(caminho + File.separator + "output.txt");
-
-		System.out.println("Done, check selected directory");
-
-	}
-
+        Generate gen = new Generate(dimX, dimY, flitWidth, numberOfPackets, warmupPcks, flitsPerPackage);
+        genSinks genS = new genSinks(dimX, dimY, flitWidth);
+        ArrayList<String> sinks = genS.doSinks(distrib, totalPcks);
+        for (Double rate : rates) {
+            System.out.println("Generating the "+rate+"% rate.");
+            String path = caminho + File.separator + "F" + String.format("%03d", rate.intValue());
+            DistTime distTime = new DistTime(flitClockCycles, flitsPerPackage, totalPcks, rate);
+            gen.writeTraffic(sinks, path, distTime.uniform());
+        }
+        gen.printNofPcks(caminho + File.separator + "output.txt");
+        System.out.println("Done, check selected directory");
+    }
 }
